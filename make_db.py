fcab5ab39e8c420527484d09686d82f87fac8f9d
chrispla <cplachouras@gmail.com>
2024-04-11 21:32:31 +0100

    db creation

--- make_db.py
+++ make_db.py
@@ -1,26 +1,41 @@
 """Do inference and save embeddings to a dir."""
 
+import argparse
 from pathlib import Path
 
 import torch
+from tqdm import tqdm
 
 from dataset import AudioDB
 from model import VGGlike
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--ckpt", "-c", type=str, required=True)
+parser.add_argument("--in_dir", "-i", type=str, default="data/database_recordings")
+parser.add_argument("--out_dir", "-o", type=str, default="db/")
+args = parser.parse_args()
+
+Path(args.out_dir).mkdir(exist_ok=True)
+
+batch_size = 16
+
 model = VGGlike()
-dataset = AudioDB(root="data/database_recordings", augmentations=False)
-dataloader = dataset.get_loader(batch_size=16, shuffle=False, num_workers=8)
+dataset = AudioDB(root=args.in_dir, augmentations=False)
+dataloader = dataset.get_loader(batch_size=batch_size, shuffle=False, num_workers=8)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 model.eval()
 
-emb_dir = Path("db")
+emb_dir = Path("db/fp/")
 emb_dir.mkdir(exist_ok=True)
 
-for i, batch in enumerate(dataloader):
+for i, batch in tqdm(
+    enumerate(dataloader), total=len(dataloader), leave=True, ncols=80
+):
     x = batch
     x = x.to(device)
     with torch.no_grad():
         y = model(x)
     for j, emb in enumerate(y):
-        torch.save(emb, emb_dir / f"{i}_{j}.pt")
+        # save using emb index to be able to retrieve segment during query
+        torch.save(emb, emb_dir / f"{i*16+j}.pt")
